2cc67ee7a067c13707230abab33561a58f808f3c
rishikpulhani <rishikpulhani@gmail.com>
2025-01-24 15:15:35 +0530

    refactoring

--- src/helper.rs
+++ src/helper.rs
@@ -1,4 +1,14 @@
 use std::fs;
+use std::path;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+
+use crate::cli::*;
+use crate::mutations::*;
+use crate::tests::*;
 
 pub fn clear() {
     let rm_dir_paths = fs::read_dir("./").unwrap();
@@ -15,3 +25,73 @@ pub fn clear() {
         fs::remove_dir_all(p.unwrap().path()).unwrap();
     }
 }
+
+pub fn run_tool(dir_path: &'static str, ctrlc_caller: Arc<AtomicBool>) {
+    let paths = fs::read_dir(dir_path).unwrap();
+    let mut handles = Vec::new();
+
+    for path_ in paths {
+        let r1 = ctrlc_caller.clone();
+        handles.push(thread::spawn(move || {
+            let path = path_.unwrap().path();
+            let new_file = PathBuf::from(path.clone());
+            let file_name = new_file.file_name().unwrap().to_str().unwrap(); //this gives the filename of the file currently being mutatated
+            let file_name_without_extension = file_name.split(".").collect::<Vec<&str>>()[0];
+            let file_path = format!("{dir_path}/{}", file_name);
+            let tmp_file_name = format!("{dir_path}/{}tmp.sol", file_name);
+
+            //mutant generation
+            mutate(&path, &tmp_file_name);
+            let mutants = fs::read_dir(format!(
+                "./gambit_out_{}/mutants",
+                file_name_without_extension
+            ))
+            .unwrap();
+            //fs::read_dir("./gambit_out/mutants").unwrap();
+
+            //tests running
+            //looping
+            for mutant in mutants {
+                if r1.load(Ordering::SeqCst) {
+                    let mutant_check = mutant.as_ref().unwrap().path();
+                    let mutant_dir = mutant.as_ref().unwrap().file_name().into_string().unwrap();
+                    run_tests(&mutant_dir, &mutant_check, &path);
+                //generate_output(&mutant_dir, &path)
+                } else {
+                    break;
+                }
+            }
+
+            let _ = fs::copy(Path::new(&tmp_file_name), Path::new(&file_path));
+            fs::remove_file(tmp_file_name).unwrap();
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+pub fn print_result(dir_path: &'static str) {
+    let paths = fs::read_dir(dir_path).unwrap();
+    for path_ in paths {
+        let path = path_.unwrap().path();
+        let new_file = PathBuf::from(path.clone());
+        let file_name = new_file.file_name().unwrap().to_str().unwrap(); //this gives the filename of the file currently being mutatated
+        let file_name_without_extension = file_name.split(".").collect::<Vec<&str>>()[0];
+        let mutants = fs::read_dir(format!(
+            "./gambit_out_{}/mutants",
+            file_name_without_extension
+        ))
+        .unwrap();
+        for mutant in mutants {
+            let mutant_dir = mutant.as_ref().unwrap().file_name().into_string().unwrap();
+            //println!("results for {}",file_name);
+            generate_output(&mutant_dir, &path)
+        }
+    }
+}
+
+
+
+// change the beskar out folder division to fill folder wise datat from remappings
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 
-use Beskar::{clear, generate_output, mutate, run_tests};
+use Beskar::{clear, generate_output, print_result, run_tool};
 
 /// 1. terminal report overview
 /*
@@ -31,10 +31,7 @@ fn main() {
             // removal of earlier files due to previuos iteration if the tool
             clear();
 
-            // mutant generation and testing of tests
-            let paths = fs::read_dir("./src").unwrap();
-            let mut handles = Vec::new();
-            ///let mut ctrlc_handlers = Vec::new();
+            //ctrlc handler
             let running = Arc::new(AtomicBool::new(true));
             let r = running.clone();
             ctrlc::set_handler(move || {
@@ -42,70 +39,12 @@ fn main() {
             })
             .expect("Error setting Ctrl-C handler");
 
-            for path_ in paths {
-                let r1 = running.clone();
-                handles.push(thread::spawn(move || {
-                    let path = path_.unwrap().path();
-                    let new_file = PathBuf::from(path.clone());
-                    let file_name = new_file.file_name().unwrap().to_str().unwrap(); //this gives the filename of the file currently being mutatated
-                    let file_name_without_extension =
-                        file_name.split(".").collect::<Vec<&str>>()[0];
-                    let file_path = format!("./src/{}", file_name);
-                    let tmp_file_name = format!("./src/{}tmp.sol", file_name);
-
-                    //mutant generation
-                    mutate(&path, &tmp_file_name);
-                    let mutants = fs::read_dir(format!(
-                        "./gambit_out_{}/mutants",
-                        file_name_without_extension
-                    ))
-                    .unwrap();
-                    //fs::read_dir("./gambit_out/mutants").unwrap();
-
-                    //tests running
-                    //looping
-                    for mutant in mutants {
-                        if r1.load(Ordering::SeqCst) {
-                            let mutant_check = mutant.as_ref().unwrap().path();
-                            let mutant_dir =
-                                mutant.as_ref().unwrap().file_name().into_string().unwrap();
-                            run_tests(&mutant_dir, &mutant_check, &path);
-                        //generate_output(&mutant_dir, &path)
-                        } else {
-                            break;
-                        }
-                    }
-
-                    let _ = fs::copy(Path::new(&tmp_file_name), Path::new(&file_path));
-                    fs::remove_file(tmp_file_name).unwrap();
-                }));
-            }
-
-            for handle in handles {
-                handle.join().unwrap();
-            }
+            // mutant generation and testing of tests
+            run_tool("./src", running.clone());
 
             //output generation
             if running.load(Ordering::SeqCst) {
-                let paths = fs::read_dir("./src").unwrap();
-                for path_ in paths {
-                    let path = path_.unwrap().path();
-                    let new_file = PathBuf::from(path.clone());
-                    let file_name = new_file.file_name().unwrap().to_str().unwrap(); //this gives the filename of the file currently being mutatated
-                    let file_name_without_extension =
-                        file_name.split(".").collect::<Vec<&str>>()[0];
-                    let mutants = fs::read_dir(format!(
-                        "./gambit_out_{}/mutants",
-                        file_name_without_extension
-                    ))
-                    .unwrap();
-                    for mutant in mutants {
-                        let mutant_dir =
-                            mutant.as_ref().unwrap().file_name().into_string().unwrap();
-                        //println!("results for {}",file_name);
-                        generate_output(&mutant_dir, &path)
-                    }
-                }
+                print_result("./src");
             } else {
                 clear();
             }
